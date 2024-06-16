447c21cff34c7e5db4d1be2e11a2b9e4f242e2b6
ChaitanyanathG <chaitanyanadhgangisetty@gmail.com>
2024-06-16 15:23:24 +0530

    Initial Commit

--- Ex_11062024/Lab001.py
+++ Ex_11062024/Lab001.py
@@ -1,1 +1,12 @@
 print("Hello World")
+print("I am a Good Person", end='_ ')
+print("I am a Bad Person")
+
+#firstname= input("what is your first name?")
+#lastname= input("what is your last name?")
+
+
+#print("Your name is ", firstname + lastname)
+
+name= "this is a string"
+print(type(name))
